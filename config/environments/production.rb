073af14b987777c93f1ef80fe838b83a000797fa
Ubuntu <gdchot@hotmail.com>
2020-03-28 03:51:09 +0000

    heroku sendgrid issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,14 +61,14 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'www.radiant-castle-78640.herokuapp.com'
+  host = 'radiant-castle-78640.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
+    :authentication => "plain",
+    :user_name      => "app166085220@heroku.com",
+    :password       => "jpjr2n3h1284",
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
