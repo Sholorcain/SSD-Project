94bea5ec9508b2d5403ed3e508a2d2fa5138e64c
Ubuntu <gdchot@hotmail.com>
2020-03-28 03:04:05 +0000

    bug fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,9 +59,9 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
- config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'radiant-castle-78640.herokuapp.com'
+  host = 'www.radiant-castle-78640.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
