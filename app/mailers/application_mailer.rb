c9259e66032709355b9b686b44f41592abc37d59
Ubuntu <gdchot@hotmail.com>
2020-03-28 05:07:54 +0000

    bug...

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "noreply@example.com"
+  default from: "gdchot@hotmail.com"
   layout 'mailer'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,22 +60,22 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
-  #config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'radiant-castle-78640.herokuapp.com' }
   #config.action_mailer.default_url_options = { host: host }
-  #ActionMailer::Base.smtp_settings = {
-  #  :address        => 'smtp.sendgrid.net',
-  #  :port           => '587',
-  #  :authentication => :plain,
-  #  :user_name      => ENV['SENDGRID_USERNAME'],
-  #  :password       => ENV['SENDGRID_PASSWORD'],
-  #  :domain         => 'heroku.com',
-  #  :enable_starttls_auto => true
-  #}
-aws_credentials = Aws::Credentials.new('AKIAJPFAGGJHYRCVQ5JA', '+fRWDnwQQStuX/nKBQQUYZIj35/bvsR/JXxrcFB3')
-Aws::Rails.add_action_mailer_delivery_method(:aws_ses, credentials: aws_credentials, region: 'eu-west-1')
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+#aws_credentials = Aws::Credentials.new('AKIAJPFAGGJHYRCVQ5JA', '+fRWDnwQQStuX/nKBQQUYZIj35/bvsR/JXxrcFB3')
+#Aws::Rails.add_action_mailer_delivery_method(:aws_ses, credentials: aws_credentials, region: 'eu-west-1')
 
-config.action_mailer.delivery_method = :aws_ses
+#config.action_mailer.delivery_method = :aws_ses
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
