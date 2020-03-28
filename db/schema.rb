a1410a69a7fdc2233d6e4faf8948fd77d37f80ba
Ubuntu <gdchot@hotmail.com>
2020-03-28 02:22:18 +0000

    data base update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200328001302) do
+ActiveRecord::Schema.define(version: 20200328004440) do
 
   create_table "items", force: :cascade do |t|
     t.string   "title"
