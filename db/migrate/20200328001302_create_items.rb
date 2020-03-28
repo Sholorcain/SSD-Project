121c8a7d9ca3c39f41413439d52991c96a4c7639
Sholorcain <54514850+Sholorcain@users.noreply.github.com>
2020-03-28 01:10:55 +0000

    Items added

---
+++ db/migrate/20200328001302_create_items.rb
@@ -0,0 +1,14 @@
+class CreateItems < ActiveRecord::Migration[5.0]
+  def change
+    create_table :items do |t|
+      t.string :title
+      t.string :author
+      t.text :description
+      t.decimal :price
+      t.string :image_url
+      t.string :category
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200327051142) do
+ActiveRecord::Schema.define(version: 20200328001302) do
+
+  create_table "items", force: :cascade do |t|
+    t.string   "title"
+    t.string   "author"
+    t.text     "description"
+    t.decimal  "price"
+    t.string   "image_url"
+    t.string   "category"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
