d5e19902b04afcfb55d65945d4ab80f8bee1abd0
Sholorcain <54514850+Sholorcain@users.noreply.github.com>
2020-03-31 20:57:02 +0100

    Added category filters

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
+    @categories = Category.all
   end
-
+  
+  def category
+    catName = params[:title]
+    @items = Item.where("category like ? ", catName)
+  end
+  
   def help
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :categories
   resources :items
   get 'sessions/new'
 
@@ -6,6 +7,8 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+  
+  get 'category/:title', to: 'static_pages#category'
   
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
