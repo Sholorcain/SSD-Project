cef95915b854f93e3fa32429b689ef38604bbd59
DConnell1011100 <danielconnell2012@gmail.com>
2020-04-14 11:25:53 +0000

    added nifty modal for item added to cart

--- app/assets/javascripts/addToCartAJAX.js
+++ app/assets/javascripts/addToCartAJAX.js
@@ -1,7 +1,15 @@
-function loadDoc(itemID, itemTitle) {
+var itemTitle;
+var itemID;
 
+function confirmAdd(itemID, itemTitle) {
+    this.itemTitle = itemTitle;
+    this.itemID = itemID;
     var xhttp = new XMLHttpRequest();
     xhttp.open("GET", "cart/" + itemID, true);
     xhttp.send();
-    alert(itemTitle + ' was added to your cart.');
+    //alert(itemTitle + ' was added to your cart.');
+}
+
+function getCurrentTitle() {
+    return itemTitle;
 }
