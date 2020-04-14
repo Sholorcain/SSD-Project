4a6d2b8c718b08d25cf90c2681a84d014bdc62c8
golpochat <sujanhossain313@gmail.com>
2020-04-14 13:46:55 +0000

    cart icon introduce

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
-      redirect_to @user
+      redirect_to current_user
     else
       render 'edit'
     end
