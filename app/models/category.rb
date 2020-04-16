70847f42d23aa1e38d9d8279cdf94b6c50c134fe
golpochat <sujanhossain313@gmail.com>
2020-04-16 22:32:40 +0000

    create unique category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ApplicationRecord
-    validates :title, presence:true, length: {minimum: 5, maximum:18}
-    validates :description, presence:true, length: {minimum: 10, maximum:300}
+    validates :title, presence:true, uniqueness: true
+    validates :description, presence:true
 end
