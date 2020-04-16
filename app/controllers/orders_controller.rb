4eacd1de64da134eb01f723bb00de977e7cfe4fc
Ubuntu <gdchot@hotmail.com>
2020-04-16 21:52:02 +0000

    Removed second call for notice

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -40,7 +40,7 @@ class OrdersController < ApplicationController
 
     respond_to do |format|
       if @order.save
-        format.html { redirect_to @order, notice: 'Order was successfully created.' }
+        format.html { redirect_to @order, 'Order was successfully created.' }
         format.json { render :show, status: :created, location: @order }
       else
         format.html { render :new }
@@ -54,7 +54,7 @@ class OrdersController < ApplicationController
   def update
     respond_to do |format|
       if @order.update(order_params)
-        format.html { redirect_to @order, notice: 'Order was successfully updated.' }
+        format.html { redirect_to @order, 'Order was successfully updated.' }
         format.json { render :show, status: :ok, location: @order }
       else
         format.html { render :edit }
@@ -68,7 +68,7 @@ class OrdersController < ApplicationController
   def destroy
     @order.destroy
     respond_to do |format|
-      format.html { redirect_to orders_url, notice: 'Order was successfully destroyed.' }
+      format.html { redirect_to orders_url, 'Order was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
