8a24be5a338e7e98eec05f96c014397394dd32f8
DConnell1011100 <danielconnell2012@gmail.com>
2020-04-16 09:39:27 +0000

    cleaned up ajax file

--- app/assets/javascripts/addToCartAJAX.js
+++ app/assets/javascripts/addToCartAJAX.js
@@ -5,14 +5,11 @@ function confirmAdd(itemID, itemTitle) {
     this.itemTitle = itemTitle;
     this.itemID = itemID;
     var xhttp = new XMLHttpRequest();
+
+    //send HTTP get request via AJAX that is asynchronous to increment cart/itemID
     xhttp.open("GET", "cart/" + itemID, true);
     xhttp.send();
-    //alert(itemTitle + ' was added to your cart.');
-    setTimeout(function(){// wait for 5 secs(2)
-           location.reload(); // then reload the page.(3)
-      }, 3000); 
-}
-
-function getCurrentTitle() {
-    return itemTitle;
+    setTimeout(function() { // wait for 5 secs(2)
+        location.reload(); // then reload the page.(3)
+    }, 3000);
 }
