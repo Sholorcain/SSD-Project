3b6276cfccba1e97d8b878218b088da667412dfc
Ubuntu <gdchot@hotmail.com>
2020-04-16 23:37:50 +0000

    added more flexible items to the featured items on home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,9 @@ class StaticPagesController < ApplicationController
   def home
     @categories = Category.all
     @items = Item.all
+    @itemfeatured1 = Item.find_by_id(20)
+    @itemfeatured2 = Item.find_by_id(2)
+    @itemfeatured3 = Item.find_by_id(19)
     
     @new_arrival = Item.order(created_at: :asc).last(5)
   end
