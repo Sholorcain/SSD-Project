07b4fce89e4a4f45b7bc697b69838f9b06bba8dc
golpochat <sujanhossain313@gmail.com>
2020-04-16 16:11:44 +0000

    fix contact page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200410001842) do
+ActiveRecord::Schema.define(version: 20200416155150) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "title"
@@ -18,6 +18,11 @@ ActiveRecord::Schema.define(version: 20200410001842) do
     t.string   "image"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "items", force: :cascade do |t|
