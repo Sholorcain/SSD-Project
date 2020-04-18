770501ce61fbd56121a897cff3a83aea68db3186
golpochat <sujanhossain313@gmail.com>
2020-04-18 10:02:55 +0000

    add authorization to sales method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,12 @@ class UsersController < ApplicationController
 
 
   def show
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
+    if current_user.admin?
+      @user = User.all
+    else
+      @user = User.find(params[:id])
+    end
   end
 
   def new
