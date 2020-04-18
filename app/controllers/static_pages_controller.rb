68bcd248ac7a1d65421a54503d8669b6cfa13196
Ubuntu <54514850+Sholorcain@users.noreply.github.com>
2020-04-18 13:00:15 +0000

    Changed featured items so that they differ from new arrivals

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,9 +2,9 @@ class StaticPagesController < ApplicationController
   def home
     @categories = Category.all
     @items = Item.all
-    @itemfeatured1 = Item.find_by_id(20)
+    @itemfeatured1 = Item.find_by_id(18)
     @itemfeatured2 = Item.find_by_id(2)
-    @itemfeatured3 = Item.find_by_id(19)
+    @itemfeatured3 = Item.find_by_id(7)
     
     @new_arrival = Item.order(created_at: :asc).last(5)
   end
