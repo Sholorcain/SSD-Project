40d51035342e23a6a229ff2f2586b3b4530e1f3c
golpochat <sujanhossain313@gmail.com>
2020-04-18 00:02:15 +0000

    fix css issue on cart, checkout, paypal link, add pagiantion to order page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,9 +5,9 @@ class OrdersController < ApplicationController
   # GET /orders.json
   def index
     if current_user.admin?
-      @orders = Order.all
+      @orders = Item.all.paginate(:page => params[:page], per_page: 10)
     else
-      @orders = Order.where(user_id: current_user.id)
+       @orders = Order.all.where(user_id: current_user.id).paginate(:page => params[:page], per_page: 10)
     end
   end
   
